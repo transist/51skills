3040028660d48b8434a81a280d6abe6409eabdba
Scott Ballantyne <ussballantyne@gmail.com>
2012-05-07 11:49:59 +0800

    another try

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,8 @@
-if Rails.env == "production"
-  # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "tedxshanghai"}
-else
-  # get credentials from YML file
-  S3_CREDENTIALS = YAML::load(File.open(Rails.root.join("config/s3.yml")))
-end
+# if Rails.env == "production"
+#   # set credentials from ENV hash
+#   S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "tedxshanghai"}
+# else
+#   # get credentials from YML file
+#   
+# end
+S3_CREDENTIALS = YAML::load(File.open(Rails.root.join("config/s3.yml")))
