412ce9e5a392a296441748cde3f12f85af373381
Scott Ballantyne <ussballantyne@gmail.com>
2012-05-10 23:52:44 +0800

    trying to fix the sync

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -3,7 +3,7 @@ class Slide < ActiveRecord::Base
   attr_accessor :width, :height
   belongs_to :presentation
   has_attached_file :image, :url => "/system/:hash.:extension", :storage => :s3, 
-                            :hash_data => ":class/:attachment/:id/:style/:created_at",
+                              :hash_data => ":class/:attachment/:id/:style/:created_at",
                             :styles => { :original => '940x680#', :full => '730x500#', :pin => '300x300#', :thumb => '100x100#', :tiny => '50x50#'},
                             :bucket => YAML::load(File.open(Rails.root.join("config/s3.yml")))[Rails.env][:bucket], 
                             :s3_credentials => YAML::load(File.open(Rails.root.join("config/s3.yml"))), 
