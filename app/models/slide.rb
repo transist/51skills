8de35c544c2b9c80078f4e6e2466683b36a2670f
Scott Ballantyne <ussballantyne@gmail.com>
2012-05-10 16:22:19 +0800

    making the slideshow working

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -3,10 +3,11 @@ class Slide < ActiveRecord::Base
   attr_accessor :width, :height
   belongs_to :presentation
   has_attached_file :image, :url => "/system/:hash.:extension", :storage => :s3, 
+                            :hash_data => ":class/:attachment/:id/:style",
                             :styles => lambda { |slide| presentation = slide.instance
                                                         dimensions =  [presentation.width, 
-                                                        presentation.height].join("x") + ">" 
-                                                        { :original => dimensions, :thumb => '250x175>' 
+                                                        presentation.height].join("x") + "#" 
+                                                        { :original => dimensions, :thumb => '250x175#' 
                                                       }},
                             :bucket => YAML::load(File.open(Rails.root.join("config/s3.yml")))[Rails.env][:bucket], 
                             :s3_credentials => YAML::load(File.open(Rails.root.join("config/s3.yml"))), 
