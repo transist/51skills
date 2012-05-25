6633fcc7bd465c752415e7cff9b2b0d9b90dc1ed
Scott Ballantyne <ussballantyne@gmail.com>
2012-05-25 11:29:23 +0800

    removing bs migrations

--- db/migrate/20120512080334_add_box_size_to_pane.rb
+++
@@ -1,5 +0,0 @@
-class AddBoxSizeToPane < ActiveRecord::Migration
-  def change
-    add_column :panes, :box_size, :string
-  end
-end
--- db/migrate/20120512080914_add_nested_set_columns_to_pane.rb
+++
@@ -1,8 +0,0 @@
-class AddNestedSetColumnsToPane < ActiveRecord::Migration
-  def change
-    add_column :panes, :parent_id, :integer
-    add_column :panes, :depth, :integer
-    add_column :panes, :lft, :integer
-    add_column :panes, :rgt, :integer
-  end
-end
