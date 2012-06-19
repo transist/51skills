2562eccd99db9ae94ecb0ba95b954f94c10a8aa6
simsicon <simsicon@gmail.com>
2012-06-19 16:49:16 +0800

    datetime

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,11 @@
 class ProfilesController < ApplicationController
   def edit
-    person = Person.find params[:person_id]
-    person.profile.nil? ? @profile = Profile.new : @profile = person.profile
+    @person = Person.find params[:person_id]
+    if @person.profile.nil?
+      profile = Profile.create
+      @person.profile = profile
+    end
+    @profile = @person.profile
   end
 
   def update
