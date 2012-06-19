adc7911be5ea9a3a8209ca3a4a64505db8908842
simsicon <simsicon@gmail.com>
2012-06-19 18:30:25 +0800

    fix avatar storage

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,12 @@
 class Profile < ActiveRecord::Base
   attr_accessible :about, :person_id, :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>", :tiny => '50x50>'}
+  
+  has_attached_file :avatar, :url => "/system/:hash/:style.:extension", :storage => :s3, 
+                            :hash_data => ":class/:attachment/:id/",
+                            :styles => { :medium => "300x300>", :thumb => "100x100>", :tiny => '50x50>'},
+                            :bucket => YAML::load(File.open(Rails.root.join("config/s3.yml")))[Rails.env][:bucket], 
+                            :s3_credentials => YAML::load(File.open(Rails.root.join("config/s3.yml"))), 
+                            :hash_secret => "longSecretS asdas das tring"
 
   
 end
