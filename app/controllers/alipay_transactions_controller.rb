647d22b8f1949423c9af348302815412698c8088
Rainux Luo <rainux@gmail.com>
2012-07-27 17:46:57 +0800

    Copy alipay_transactions controller from pay_fu

--- app/controllers/alipay_transactions_controller.rb
+++ app/controllers/alipay_transactions_controller.rb
@@ -1,9 +1,26 @@
-
 class AlipayTransactionsController < PayFu::AlipayTransactionsController
+  include ActiveMerchant::Billing::Integrations
 
   def notify
-    super
+    notify = Alipay::Notification.new(request.raw_post)
+    if notify.acknowledge
+      if transaction = PayFu::AlipayTransaction.find_by_transaction_id(notify.trade_no)
+        transaction.update_attributes(transaction_attributes(notify))
+      else
+        PayFu::AlipayTransaction.create(transaction_attributes(notify))
+      end
+    end
+    render :nothing => true
   end
 
+  def transaction_attributes(notify)
+    @transaction_attributes ||= {
+      :transaction_id => notify.trade_no,
+      :transaction_type => notify.payment_type,
+      :payment_status => notify.trade_status,
+      :payment_date => notify.notify_time,
+      :gross => notify.total_fee,
+      :raw_post => notify.raw
+    }
+  end
 end
-
